da21e19adc4225dabe1cf75455d3f4a4e5e83999
Dreaw Riddleton <dreawriddleton@hotmail.com>
2021-12-07 21:51:29 +0100

    Small changes to player and some scripts cleanup

--- ShopGame/Assets/Scripts/Actions/Movement.cs
+++ ShopGame/Assets/Scripts/Actions/Movement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -33,15 +31,10 @@ namespace Game.Actions
             m_Rigid = GetComponent<Rigidbody>();
         }
 
-        public void Move(InputAction.CallbackContext context)
-        {
-            //Vector3 _Input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        }
         private void Update()
         {
             Vector2 _Input = controlsManager.Player.Move.ReadValue<Vector2>();
             m_Rigid.position += new Vector3(_Input.x, 0, _Input.y) * Time.deltaTime * m_SpeedModifier;
-            ;
         }
 
     }
