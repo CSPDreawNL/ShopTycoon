e77d68fa6f6ec477b8025da5afb5162800b9c7b4
JustinProo <justincomans.2003@gmail.com>
2021-12-07 22:34:32 +0100

    Player manager + input system
    Player manager added, this will contain all player actions related scripts and variables.
    Added Input for debug interactions.
    Movement now uses the new player manager system.
    Interactions now also use the player manager and the new input system. also interact does not work because of the debug functions.

--- ShopGame/Assets/Scripts/Actions/PlayerInteraction.cs
+++ ShopGame/Assets/Scripts/Actions/PlayerInteraction.cs
@@ -1,41 +1,50 @@
 using UnityEngine;
 
-public class PlayerInteraction : MonoBehaviour
+namespace Game.Actions
 {
-    [SerializeField] KeyCode m_interactionKey = KeyCode.E;
-    [SerializeField] KeyCode m_debugKey = KeyCode.F1;
-    [SerializeField] float m_raycastDistance = 3;
-    private void Update()
+    public class PlayerInteraction : MonoBehaviour
     {
-        if (Input.GetKeyDown(m_interactionKey))
-            Interact();
-        if (Input.GetKeyDown(m_debugKey))
-            Interact(true);
-    }
-    public void Interact(bool _debug = false)
-    {
-        RaycastHit _whatHit;
-        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward) * m_raycastDistance, out _whatHit);
-        if (_whatHit.transform != null)
-            CheckForInteraction(_whatHit, _debug);
-    }
+        [SerializeField] Management.PlayerManager manager;
 
-    void CheckForInteraction(RaycastHit _hit, bool _debug = false)
-    {
-        var _interactible = _hit.transform.GetComponent<Interactible>();
-        if (_interactible)
+        [SerializeField] GameObject camera;
+        private void Update()
         {
-            if (!_debug)
-                _interactible.Interact();
-            else if (_debug)
-                _interactible.DebugFunction();
+            // check input and either interact or debug interact
+            if (manager.controls.Player.Interact.triggered)
+                Debug.Log("INTERACTING");
+            //Interact();
+            if (manager.controls.Player.DebugInteract.triggered)
+                Debug.Log("DEBUGGING");
+            //Interact(true);
         }
-    }
+        public void Interact(bool _debug = false)
+        {
+            // shoot a raycast and check if it hits anything
+            RaycastHit _whatHit;
+            Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward) * manager.raycastDistance, out _whatHit);
+            if (_whatHit.transform != null)
+                CheckForInteraction(_whatHit, _debug);
+        }
 
+        void CheckForInteraction(RaycastHit _hit, bool _debug = false)
+        {
+            // if the object hit can be interacted with
+            var _interactible = _hit.transform.GetComponent<Interactible>();
+            if (_interactible)
+            {
+                // interact or debug interact
+                if (!_debug)
+                    _interactible.Interact();
+                else if (_debug)
+                    _interactible.DebugFunction();
+            }
+        }
 #if UNITY_EDITOR
-    private void OnDrawGizmos()
-    {
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * m_raycastDistance, Color.red);
-    }
+        private void OnDrawGizmos()
+        {
+            // draw a ray from the camera, forwards
+            Debug.DrawRay(camera.transform.position, transform.TransformDirection(Vector3.forward) * manager.raycastDistance, Color.red);
+        }
 #endif
+    }
 }
